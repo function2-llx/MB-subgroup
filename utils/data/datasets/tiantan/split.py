33faf0710138f15eaeb6831b7bdab2147e485721
function2 <function2@qq.com>
2021-06-01 18:22:15 +0800

    print folds table

--- utils/data/datasets/tiantan/split.py
+++ utils/data/datasets/tiantan/split.py
@@ -4,27 +4,35 @@ import json
 import random
 from collections import OrderedDict
 
-patients = {}
+import numpy as np
+import pandas as pd
+
+grouped_patients = {}
 n_folds = 3
 folds = [[] for _ in range(n_folds)]
 random.seed(233)
+subgroups = ['WNT', 'SHH', 'G3', 'G4']
 
 if __name__ == '__main__':
     cohort = json.load(open('cohort.json'))
-    patients = OrderedDict([(group, [patient for patient in cohort if patient['subgroup'] == group]) for group in ['WNT', 'SHH', 'G3', 'G4']])
-
-    for subgroup_patients in patients.values():
-        q, r = divmod(len(subgroup_patients), n_folds)
+    # group by subtypes
+    grouped_patients = [[patient for patient in cohort if patient['subgroup'] == group] for group in subgroups]
+    counts = np.empty((len(subgroups), n_folds), dtype=int)
+    for subgroup_id, patients in enumerate(grouped_patients):
+        q, r = divmod(len(patients), n_folds)
         assert q >= 1
-        random.shuffle(subgroup_patients)
+        random.shuffle(patients)
         chunks = [q] * (n_folds - r) + [q + 1] * r
         random.shuffle(chunks)
-        for i in range(1, len(chunks)):
-            chunks[i] += chunks[i - 1]
+        for fold_id in range(1, len(chunks)):
+            chunks[fold_id] += chunks[fold_id - 1]
         chunks = [0] + chunks
-        for i, (l, r) in enumerate(zip(chunks, chunks[1:])):
-            folds[i].extend(subgroup_patients[l:r])
+
+        for fold_id, (l, r) in enumerate(zip(chunks, chunks[1:])):
+            folds[fold_id].extend(patients[l:r])
+            counts[subgroup_id, fold_id] = r - l
 
     print([len(fold) for fold in folds])
 
     json.dump(folds, open(f'folds-{n_folds}.json', 'w'), indent=4, ensure_ascii=False)
+    df = pd.DataFrame(counts, columns=[f'第{i}折' for i in range(1, 4)], index=subgroups)
