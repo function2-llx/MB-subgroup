c0335e6d27023b1c0cab0888b7858e6131f70fe6
function2 <luolx21@mails.tsinghua.edu.cn>
2021-12-14 18:32:07 +0800

    don't save too much

--- finetune.py
+++ finetune.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import shutil
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Optional
@@ -41,7 +42,8 @@ class Finetuner(FinetunerBase):
     def run_fold(self, val_id: int):
         fold_output_dir = Path(self.args.output_dir) / f'val-{val_id}'
         fold_output_dir.mkdir(exist_ok=True, parents=True)
-        best_checkpoint_link = fold_output_dir / 'checkpoint-best'
+        best_checkpoint_path = fold_output_dir / 'checkpoint-best.pth.tar'
+        latest_checkpoint_path = fold_output_dir / 'checkpoint-latest.pth.tar'
         in_channels = len(self.args.protocols) + len(self.args.seg_inputs)
         if self.args.input_fg_mask:
             in_channels += 1
@@ -112,8 +114,13 @@ class Finetuner(FinetunerBase):
                     # if self.is_world_master() == 0:
                     writer.flush()
 
-                checkpoint_save_dir = fold_output_dir / f'checkpoint-ep{epoch}'
-                val_output = self.run_eval(model, val_set, reporter=self.epoch_reporters[epoch]['cross-val'], plot_dir=checkpoint_save_dir, plot_num=3)
+                val_output = self.run_eval(
+                    model,
+                    val_set,
+                    reporter=self.epoch_reporters[epoch]['cross-val'],
+                    plot_dir=self.epoch_reporters[epoch]['cross-val'].report_dir,
+                    plot_num=3
+                )
                 val_loss = self.combine_loss(val_output.cls_loss, val_output.seg_loss)
                 if epoch > 0:
                     scheduler.step(val_loss)
@@ -122,23 +129,17 @@ class Finetuner(FinetunerBase):
                 if not plot_skip_first or epoch > 0:
                     val_outputs.append(val_output)
 
-                # if self.args.patience == 0:
-                if self.args.save_strategy is IntervalStrategy.EPOCH:
-                    checkpoint_save_dir = fold_output_dir / f'checkpoint-ep{epoch}'
-                else:
-                    raise ValueError
-                if val_loss < best_loss:
-                    best_loss = val_loss
-                    best_checkpoint_link.unlink(missing_ok=True)
-                    best_checkpoint_link.symlink_to(f'checkpoint-ep{epoch}')
-                checkpoint_save_dir.mkdir(parents=True, exist_ok=True)
                 save_states = {
                     'state_dict': model.state_dict(),
                     'optimizer': optimizer.state_dict(),
                     'scheduler': scheduler.state_dict(),
                 }
-                torch.save(save_states, checkpoint_save_dir / 'states.pth')
-                logging.info(f'save checkpoint to {checkpoint_save_dir}\n')
+                torch.save(save_states, latest_checkpoint_path)
+                logging.info(f'save latest checkpoint to {latest_checkpoint_path}\n')
+                if val_loss < best_loss:
+                    best_loss = val_loss
+                    shutil.copy2(latest_checkpoint_path, best_checkpoint_path)
+                    logging.info(f'copy best checkpoint to {best_checkpoint_path}\n')
 
             pd.DataFrame({
                 f'epoch{epoch}': epoch_reporters['cross-val'].digest()
@@ -166,8 +167,15 @@ class Finetuner(FinetunerBase):
             num_classes=len(self.args.subgroups),
             num_pretrain_seg=self.args.num_pretrain_seg,
         )
-        model.load_state_dict(torch.load(best_checkpoint_link / 'states.pth')['state_dict'])
-        self.run_eval(model, val_set, self.reporters['cross-val'], plot_num=len(val_set), plot_dir=Path(self.args.output_dir) / 'seg-outputs')
+        model.load_state_dict(torch.load(best_checkpoint_path)['state_dict'])
+        self.run_eval(
+            model,
+            val_set,
+            self.reporters['cross-val'],
+            plot_num=len(val_set),
+            plot_dir=Path(self.args.output_dir) / 'seg-outputs',
+            save_dir=Path(self.args.output_dir) / 'seg-outputs',
+        )
 
     def run_eval(
         self,
@@ -176,6 +184,7 @@ class Finetuner(FinetunerBase):
         reporter: Optional[Reporter] = None,
         plot_num=0,
         plot_dir: Path = None,
+        save_dir: Path = None,
     ) -> EvalOutput:
         model.eval()
         post_trans = Compose(
@@ -211,59 +220,60 @@ class Finetuner(FinetunerBase):
                     reporter.append(data, logit[0], meandice)
                 meandices.append(meandice)
                 step += 1
+                patient = data['patient'][0]
+                if idx in plot_idx:
+                    patient_plot_dir = plot_dir / patient
+                    patient_plot_dir.mkdir(exist_ok=True)
+                    fig, ax = plt.subplots(1, 3, figsize=(16, 5))
+                    fig: Figure
+                    ax = {
+                        protocol: ax[i]
+                        for i, protocol in enumerate(self.args.protocols)
+                    }
 
-                if idx not in plot_idx:
-                    continue
-                patient_plot_dir = plot_dir / data['patient'][0]
-                patient_plot_dir.mkdir(exist_ok=True)
-                fig, ax = plt.subplots(1, 3, figsize=(16, 5))
-                fig: Figure
-                ax = {
-                    protocol: ax[i]
-                    for i, protocol in enumerate(self.args.protocols)
-                }
+                    # select a slice to plot segmentation result
+                    idx = seg_ref[0].sum(dim=(0, 1, 2)).argmax().item()
+                    for protocol in self.args.protocols:
+                        img = data[protocol][0, 0]
+                        # idx = img.shape[2] // 2
+                        ax[protocol].imshow(np.rot90(img[:, :, idx]), cmap='gray')
+                        seg_t = {
+                            ScanProtocol.T2: 'AT',
+                            ScanProtocol.T1c: 'CT',
+                        }.get(protocol, None)
+                        if seg_t is None or seg_t not in self.args.segs:
+                            continue
+                        seg_id = self.args.segs.index(seg_t)
+                        from matplotlib.colors import ListedColormap
+                        cur_seg_ref = seg_ref[0, seg_id, :, :, idx].cpu().numpy()
+                        cur_seg_pred = seg_pred[0, seg_id, :, :, idx].int().cpu().numpy()
+                        ax[protocol].imshow(np.rot90(cur_seg_pred), vmin=0, vmax=1, cmap=ListedColormap(['none', 'red']), alpha=0.5)
+                        ax[protocol].imshow(np.rot90(cur_seg_ref), vmin=0, vmax=1, cmap=ListedColormap(['none', 'green']), alpha=0.5)
+                    fig.savefig(patient_plot_dir / 'plot.pdf', dpi=300)
+                    plt.show()
+                    plt.close()
 
-                # select a slice to plot segmentation result
-                idx = seg_ref[0].sum(dim=(0, 1, 2)).argmax().item()
-                for protocol in self.args.protocols:
-                    img = data[protocol][0, 0]
-                    # idx = img.shape[2] // 2
-                    ax[protocol].imshow(np.rot90(img[:, :, idx]), cmap='gray')
-                    seg_t = {
-                        ScanProtocol.T2: 'AT',
-                        ScanProtocol.T1c: 'CT',
-                    }.get(protocol, None)
-                    if seg_t is None or seg_t not in self.args.segs:
-                        continue
-                    seg_id = self.args.segs.index(seg_t)
-                    from matplotlib.colors import ListedColormap
-                    cur_seg_ref = seg_ref[0, seg_id, :, :, idx].cpu().numpy()
-                    cur_seg_pred = seg_pred[0, seg_id, :, :, idx].int().cpu().numpy()
-                    ax[protocol].imshow(np.rot90(cur_seg_pred), vmin=0, vmax=1, cmap=ListedColormap(['none', 'red']), alpha=0.5)
-                    ax[protocol].imshow(np.rot90(cur_seg_ref), vmin=0, vmax=1, cmap=ListedColormap(['none', 'green']), alpha=0.5)
-                fig.savefig(patient_plot_dir / 'plot.pdf', dpi=300)
-                plt.show()
-                plt.close()
-
-                saver = NiftiSaver(
-                    output_dir=patient_plot_dir,
-                    output_postfix='',
-                    output_dtype=np.int8,
-                    resample=False,
-                    separate_folder=False,
-                    print_log=False,
-                )
-                seg_ref_keys = {
-                    'AT': ScanProtocol.T2,
-                    'CT': ScanProtocol.T1c,
-                }
-                for i, seg in enumerate(self.args.segs):
-                    cur_seg_pred = seg_pred[0, [seg_id]].int().cpu().numpy()
-                    seg_ref_key = seg_ref_keys[seg]
-                    saver.save(cur_seg_pred, {
-                        ImageMetaKey.FILENAME_OR_OBJ: seg,
-                        'affine': data[f'{str(seg_ref_key)}_meta_dict']['affine'][0],
-                    })
+                if save_dir is not None:
+                    patient_save_dir = save_dir / patient
+                    saver = NiftiSaver(
+                        output_dir=patient_save_dir,
+                        output_postfix='',
+                        output_dtype=np.int8,
+                        resample=False,
+                        separate_folder=False,
+                        print_log=False,
+                    )
+                    seg_ref_keys = {
+                        'AT': ScanProtocol.T2,
+                        'CT': ScanProtocol.T1c,
+                    }
+                    for i, seg in enumerate(self.args.segs):
+                        cur_seg_pred = seg_pred[0, [seg_id]].int().cpu().numpy()
+                        seg_ref_key = seg_ref_keys[seg]
+                        saver.save(cur_seg_pred, {
+                            ImageMetaKey.FILENAME_OR_OBJ: seg,
+                            'affine': data[f'{str(seg_ref_key)}_meta_dict']['affine'][0],
+                        })
         ret.cls_loss /= step
         ret.seg_loss /= step
         ret.meandice = torch.stack(meandices)
