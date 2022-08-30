35b2df87f470a4d6255e0269fcd297abd6f76eca
function2 <luolx21@mails.tsinghua.edu.cn>
2022-08-30 11:09:16 +0800

    update plan

--- scripts/plan.py
+++ scripts/plan.py
@@ -1,43 +1,60 @@
 from pathlib import Path
 
 import numpy as np
-from tqdm.contrib.concurrent import process_map
+import pandas as pd
+import nibabel as nib
 
-import monai
+data_dir = Path('origin')
+cohort = pd.read_excel(data_dir / 'patients.xlsx', sheet_name='儿童')
+cohort.set_index('姓名', inplace=True)
 
-from cls_unet.cls_unet import get_params
-from mbs.datasets.tiantan.load import read_cohort_info
+def process_patient(patient_dir: Path):
+    patient = patient_dir.name
+    if patient not in cohort.index:
+        print(patient)
 
-data_dir = Path('preprocessed')
-loader = monai.transforms.Compose([
-    monai.transforms.LoadImageD(['T2', 'AT']),
-])
-
-def process_patient(_, info):
-    patient_dir = data_dir / info['subject']
-    data = loader({'T2': patient_dir / 'T2.nii.gz', 'AT': patient_dir / 'AT.nii.gz'})
-    spacing = data['T2_meta_dict']['pixdim'][1:4]
-    size = data['T2'].shape
-    pos = data['AT'].sum(axis=(0, 1)).nonzero()[0][-1]
-    return spacing, size, pos
-
-sample_slices = 16
+    affine = None
+    shape = None
+    not_close = False
+    for img_type in ['T2', 'T1', 'T1C', 'AT', 'CT', 'ST']:
+        img_path = patient_dir / f'{img_type}.nii'
+        if not img_path.exists():
+            print(patient, img_type)
+            continue
+        img: nib.Nifti1Image = nib.load(img_path)
+        if affine is None:
+            affine = img.affine
+            shape = img.shape
+        else:
+            affine_t = img.affine
+            for i in range(3):
+                affine_t[:, i] *= img.shape[i] / shape[i]
+            if not np.allclose(affine_t, affine, atol=1e-2, rtol=1e-2):
+                not_close = True
+                print(patient, img_type)
+                print(img.shape)
+                print(affine_t)
+            if img.shape[2] != shape[2]:
+                print('???')
+    if not_close:
+        print(patient, 'T2')
+        print(affine)
+        print(shape)
+    return {
+        'name': patient,
+        **{
+            f'p{i}': np.linalg.norm(affine[:, i])
+            for i in range(3)
+        },
+        **{
+            f's{i}': shape[i]
+            for i in range(3)
+        }
+    }
 
 def main():
-    cohort_info = read_cohort_info()
-    spacings, sizes, pos = map(np.array, zip(*process_map(process_patient, *zip(*cohort_info.iterrows()))))
-    print(pos)
-
-    spacing = np.median(spacings, axis=0)
-    size = list(map(int, np.median(sizes, axis=0)))
-    size[2] = sample_slices
-    params = get_params(tuple(size), tuple(spacing), min_fmap=2)
-    print('spacing =', spacing)
-    print('size =', size)
-    print('kernels =', params.kernels)
-    print('strides =', params.strides)
-    print('depth =', len(params.kernels) - 1)
-    print('bottleneck size =', params.fmap_shapes[-1])
+    data = [process_patient(patient_dir) for patient_dir in (data_dir / 'image').iterdir()]
+    pd.DataFrame.from_records(data).to_excel('plan.xlsx', index=False)
 
 if __name__ == '__main__':
     main()
