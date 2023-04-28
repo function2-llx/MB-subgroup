9021dd4b6f69f018a411f350f56008d399d5b6d7
function2 <luolx21@mails.tsinghua.edu.cn>
2023-04-28 19:07:48 +0800

    run test

--- scripts/cls/test.py
+++ scripts/cls/test.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
-from collections.abc import Mapping, Sequence
+from collections.abc import Sequence
 from copy import deepcopy
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import itertools
-import json
 from pathlib import Path
 
 from matplotlib import pyplot as plt
@@ -12,121 +11,98 @@ import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 from sklearn.metrics import auc, roc_auc_score, roc_curve
-
 import torch
 from torch import nn
-import torchmetrics
-from torchmetrics.utilities.enums import AverageMethod
 
-from mbs.model import MBModel
-from mbs.utils.enums import MBDataKey
-from umei.utils import DataKey, UMeIParser
+from luolib.conf import parse_exp_conf
+from luolib.models import ClsModel
+from luolib.utils import DataKey, DataSplit
 
-from mbs.args import MBArgs
-from mbs.datamodule import MBDataModule
+from mbs.conf import MBClsConf
+from mbs.models import MBClsModel
+from mbs.datamodule import MBClsDataModule, load_split
+from mbs.models.lightning.cls_model import get_cls_names
 
-@dataclass
-class MBTestArgs(MBArgs):
-    p_seeds: list[int] = field(default=None)
-    p_seg_seeds: list[int] = field(default=None)
-    val_cache_num: int = field(default=0)
+@dataclass(kw_only=True)
+class MBClsTestConf(MBClsConf):
+    p_seeds: list[int]
+    include_adults: bool = False
+    eval_batch_size: int = 1
+    print_shape: bool = False
+    val_cache_num: int = 0
 
-    def __post_init__(self):
-        super().__post_init__()
-        if self.p_seg_seeds is None:
-            self.p_seg_seeds = self.p_seeds
+class MBTester(ClsModel):
+    conf: MBClsTestConf
+    models: Sequence[Sequence[MBClsModel]]
 
-class MBTester(pl.LightningModule):
-    models: nn.ModuleList | Sequence[Sequence[MBModel]]
+    @property
+    def cls_names(self):
+        return get_cls_names(self.conf.cls_scheme)
 
-    def __init__(self, args: MBTestArgs):
-        super().__init__()
-        self.args = args
+    def __init__(self, conf: MBClsTestConf):
+        super().__init__(conf)
+        wuhu = self.create_metrics(conf.num_cls_classes)
+        self.micro_metrics = wuhu
         self.models = nn.ModuleList([
             nn.ModuleList()
-            for _ in range(args.num_folds)
+            for _ in range(conf.num_folds)
         ])
-        for (seed, seg_seed), fold_id in itertools.product(zip(args.p_seeds, args.p_seg_seeds), range(args.num_folds)):
-            seg_indicator = ''
-            if args.seg_output_dir is not None:
-                seg_indicator = f'seg-{seg_seed}'
-            ckpt_path = self.args.output_dir / seg_indicator / f'run-{seed}' / f'fold-{fold_id}' / args.cls_scheme / 'last.ckpt'
-            # ckpt_path = self.args.output_dir / f'run-{seed}' / f'fold-{fold_id}' / 'cls'
-            # for filepath in ckpt_path.iterdir():
-            #     if filepath.name.startswith('best'):
-            #         ckpt_path = filepath
-            #         break
-            # else:
-            #     raise RuntimeError
-            self.models[fold_id].append(MBModel.load_from_checkpoint(ckpt_path, strict=True, args=args))
+        for seed, fold_id in itertools.product(conf.p_seeds, range(conf.num_folds)):
+            ckpt_path = self.conf.output_dir / f'run-{seed}' / f'fold-{fold_id}' / 'last.ckpt'
+            self.models[fold_id].append(MBClsModel.load_from_checkpoint(ckpt_path, strict=True, conf=conf))
             print(f'load model from {ckpt_path}')
 
-        self.metrics: Mapping[str, torchmetrics.Metric] = nn.ModuleDict({
-            k: metric_cls(task='multiclass', num_classes=self.args.num_cls_classes, average=average)
-            for k, metric_cls, average in [
-                ('auroc', torchmetrics.AUROC, AverageMethod.NONE),
-                ('recall', torchmetrics.Recall, AverageMethod.NONE),
-                ('precision', torchmetrics.Precision, AverageMethod.NONE),
-                ('f1', torchmetrics.F1Score, AverageMethod.NONE),
-                ('acc', torchmetrics.Accuracy, AverageMethod.MICRO),
-            ]
-        })
         self.case_outputs = []
-        self.test_report = {}
-        self.val_fold = None
-
-    def reset(self):
-        for k, m in self.metrics.items():
-            m.reset()
-        self.case_outputs.clear()
-        self.test_report = {}
-
-    # def on_test_epoch_start(self) -> None:
-    #     self.reset()
+        self.split = load_split()
 
     def test_step(self, batch: dict, *args, **kwargs):
         prob = None
-        if self.val_fold == 'test':
+        sample_case = batch[DataKey.CASE][0]
+        split = self.split[sample_case]
+        for case in batch[DataKey.CASE][1:]:
+            assert split == self.split[case]
+
+        if split == 'test':
             models = list(itertools.chain(*self.models))
         else:
-            models = self.models[self.val_fold]
+            models = self.models[split]
         for model in models:
-            logit = model.forward_cls(batch[DataKey.IMG], tta=self.args.do_tta)
+            logit = model.cal_logit(batch, flip=self.conf.do_tta)
             if prob is None:
                 prob = logit.softmax(dim=-1)
             else:
                 prob += logit.softmax(dim=-1)
         prob /= len(models)
-        cls = batch[DataKey.CLS]
-        for k, metric in self.metrics.items():
-            metric(prob, cls)
-        for i, case in enumerate(batch[MBDataKey.CASE]):
+        label = batch[DataKey.CLS]
+        self.accumulate_metrics(prob, label, self.cls_metrics[DataSplit.TEST])
+        if split != 'test':
+            self.accumulate_metrics(prob, label, self.micro_metrics)
+        for i, case in enumerate(batch[DataKey.CASE]):
             self.case_outputs.append({
                 'case': case,
-                'true': self.args.cls_names[cls[i].item()],
-                'pred': self.args.cls_names[prob[i].argmax().item()],
+                'split': split,
+                'true': self.cls_names[label[i].item()],
+                'pred': self.cls_names[prob[i].argmax().item()],
                 **{
-                    cls: prob[i, j].item()
-                    for j, cls in enumerate(self.args.cls_names)
+                    label: prob[i, j].item()
+                    for j, label in enumerate(self.cls_names)
                 },
                 'prob': prob[i].cpu().numpy(),
             })
 
-    def test_epoch_end(self, *args) -> None:
-        self.test_report['n'] = len(self.case_outputs)
-        self.test_report['seeds'] = self.args.p_seeds
-        self.test_report['seg_seeds'] = self.args.p_seg_seeds
-        for k, metric in self.metrics.items():
-            m = metric.compute()
-            if metric.average == AverageMethod.NONE:
-                result = {}
-                for i, sg in enumerate(self.args.cls_names):
-                    result[sg] = m[i].item()
-                result['macro avg'] = m.mean().item()
-                self.test_report[k] = result
-            else:
-                self.test_report[k] = m.item()
-        print(json.dumps(self.test_report, indent=4, ensure_ascii=False))
+    def plot_roc(self, save_dir: Path):
+        conf = self.conf
+        cls_names = self.cls_names
+        y_true = np.array([
+            self.cls_names.index(case['true'])
+            for case in self.case_outputs if case['split'] == 'test'
+        ])
+        y_score = np.array([
+            case['prob']
+            for case in self.case_outputs if case['split'] == 'test'
+        ])
+        plot_roc(y_true, y_score, cls_names, save_dir)
+
 
 def plot_roc(y_true: np.ndarray, y_score: np.ndarray, target_names: list[str], output_dir: Path):
     target_names = deepcopy(target_names)
@@ -177,69 +153,61 @@ def plot_roc_curve(y_test, y_score, model_name, output_dir):
 
     # plt.show()
 
-def run_test():
-    (output_dir := conf.output_dir / 'eval-test' / conf.cls_scheme).mkdir(parents=True, exist_ok=True)
-    tester.reset()
-    tester.val_fold = 'test'
-    trainer.test(tester, dataloaders=datamodule.test_dataloader())
-    results_df = pd.DataFrame.from_records(tester.case_outputs)
-    cls_names = conf.cls_names
-    y_true = np.array([
-        conf.cls_names.index(case['true'])
-        for case in tester.case_outputs
-    ])
-    y_score = np.array([
-        case['prob']
-        for case in tester.case_outputs
-    ])
-    if len(cls_names) == 2:
-        cls_names = [conf.cls_scheme, conf.cls_scheme]
-    plot_roc(y_true, y_score, cls_names, output_dir)
-    if len(cls_names) == 2:
-        plot_roc_curve(y_true, y_score[:, 1], 'swt', output_dir)
+# def run_test():
+#     (output_dir := conf.output_dir / 'eval-test' / conf.cls_scheme).mkdir(parents=True, exist_ok=True)
+#     tester.reset()
+#     tester.val_fold = 'test'
+#     trainer.test(tester, dataloaders=datamodule.test_dataloader())
+#     results_df = pd.DataFrame.from_records(tester.case_outputs)
+#     cls_names = conf.cls_names
+#     y_true = np.array([
+#         conf.cls_names.index(case['true'])
+#         for case in tester.case_outputs
+#     ])
+#     y_score = np.array([
+#         case['prob']
+#         for case in tester.case_outputs
+#     ])
+#     if len(cls_names) == 2:
+#         cls_names = [conf.cls_scheme, conf.cls_scheme]
+#     plot_roc(y_true, y_score, cls_names, output_dir)
+#     if len(cls_names) == 2:
+#         plot_roc_curve(y_true, y_score[:, 1], 'swt', output_dir)
 
-    results_df.to_csv(output_dir / 'test-results.csv', index=False)
-    results_df.to_excel(output_dir / 'test-results.xlsx', index=False)
-    with open(output_dir / 'test-report.json', 'w') as f:
-        json.dump(tester.test_report, f, indent=4, ensure_ascii=False)
+#     results_df.to_csv(output_dir / 'test-results.csv', index=False)
+#     results_df.to_excel(output_dir / 'test-results.xlsx', index=False)
+#     with open(output_dir / 'test-report.json', 'w') as f:
+#         json.dump(tester.test_report, f, indent=4, ensure_ascii=False)
 
-def run_cv():
-    (output_dir := conf.output_dir / 'eval-cv' / conf.cls_scheme).mkdir(parents=True, exist_ok=True)
-    tester.reset()
-    for i in range(conf.num_folds):
-        tester.val_fold = i
-        datamodule.val_id = i
-        trainer.test(tester, dataloaders=datamodule.val_dataloader(include_test=False))
-    results_df = pd.DataFrame.from_records(tester.case_outputs)
-    results_df.to_csv(output_dir / 'cv-results.csv', index=False)
-    results_df.to_excel(output_dir / 'cv-results.xlsx', index=False)
-    with open(output_dir / 'cv-report.json', 'w') as f:
-        json.dump(tester.test_report, f, indent=4, ensure_ascii=False)
-
-conf: MBTestArgs
-datamodule: MBDataModule
-trainer: pl.Trainer
-tester: MBTester
+class MBClsTestDataModule(MBClsDataModule):
+    def val_dataloader(self):
+        return super(MBClsDataModule, self).val_dataloader()
 
 def main():
-    global conf, datamodule, trainer, tester
-
-    parser = UMeIParser((MBTestArgs,), use_conf=True)
-    args = parser.parse_args_into_dataclasses()[0]
-    print(args)
-    datamodule = MBDataModule(args)
-    tester = MBTester(args)
+    conf = parse_exp_conf(MBClsTestConf)
+    datamodule = MBClsTestDataModule(conf)
+    model = MBTester(conf)
     trainer = pl.Trainer(
         logger=False,
-        num_nodes=args.num_nodes,
         accelerator='gpu',
         devices=torch.cuda.device_count(),
-        precision=args.precision,
-        benchmark=True,
     )
-
-    run_test()
-    run_cv()
+    reports = {}
+    for i in range(conf.num_folds):
+        datamodule.val_id = i
+        trainer.test(model, datamodule.val_dataloader())
+        print(f'evaluating {i}')
+        reports[f'fold-{i}'] = model.metrics_report(model.cls_metrics_test, model.cls_names)
+    reports['macro'] = pd.DataFrame(reports).T.mean().to_dict()
+    reports['micro'] = model.metrics_report(model.micro_metrics, model.cls_names)
+    trainer.test(model, datamodule.test_dataloader())
+    reports['test'] = model.metrics_report(model.cls_metrics_test, model.cls_names)
+    
+    save_dir = conf.output_dir / 'eval'
+    save_dir.mkdir(exist_ok=True)
+    pd.DataFrame(model.case_outputs).to_excel(save_dir / 'case-outputs.xlsx', freeze_panes=(1, 0))
+    model.plot_roc(save_dir)
+    pd.DataFrame(reports).to_excel(save_dir / 'report.xlsx', freeze_panes=(1, 0))
 
 if __name__ == '__main__':
     main()
