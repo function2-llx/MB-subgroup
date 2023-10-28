9ae2c5ede9df48f210bdb82a9aee290d0f4441f0
function2 <luolx21@mails.tsinghua.edu.cn>
2023-10-28 13:51:14 +0800

    update label converting

--- mbs/datamodule/seg_dm.py
+++ mbs/datamodule/seg_dm.py
@@ -40,7 +40,7 @@ class InputTransformD(mt.Transform):
         img, label = data['img'], data['seg']
         if self.as_tensor:
             img = convert_to_tensor(img)
-        return img, convert_to_tensor(label)
+        return img, convert_to_tensor(label) > 0.5
 
 @dataclass(kw_only=True)
 class SegTransConf:
