eba1ed741402c6b334554b03c68b281b7370ce85
function2 <function2-llx@outlook.com>
2025-01-08 13:57:54 +0800

    use cross product to determine slice direction

--- scripts/pp/dicom/filter-series.py
+++ scripts/pp/dicom/filter-series.py
@@ -2,8 +2,8 @@ import json
 import re
 from pathlib import Path
 
-import SimpleITK as sitk
 import cytoolz
+import numpy as np
 from tqdm import tqdm
 
 data_dir = Path('MB-data/new-test')
@@ -11,17 +11,20 @@ src_dir = data_dir / 'NIfTI'
 dst_dir = data_dir / 'NIfTI-filtered'
 dst_dir.mkdir(exist_ok=True)
 
-def is_axial(image_file: Path, tol: float = 1e-2) -> bool:
-    reader = sitk.ImageFileReader()
-    reader.SetFileName(str(image_file))
-    reader.ReadImageInformation()
-    direction = reader.GetDirection()
-    return 1 - abs(direction[0]) < tol and 1 - abs(direction[4]) < tol
+def is_axial_orient(slice_orient: tuple[float, ...], tol: float = 1e-2) -> bool:
+    direction = np.cross(slice_orient[:3], slice_orient[3:]).astype(np.float64)
+    direction /= np.linalg.norm(direction)
+    return 1 - abs(direction[2]) < tol
 
 def check_modality(meta: dict) -> bool:
     series_desc: str = meta['SeriesDescription']
     series_desc = series_desc.upper()
     return 'T1' in series_desc or 'T2' in series_desc
+
+def check_meta(meta: dict):
+    if (slice_orient := meta.get('ImageOrientationPatientDICOM')) is None or not is_axial_orient(slice_orient):
+        return False
+    return check_modality(meta)
 
 def main():
     for study_dir in tqdm(list(src_dir.iterdir()), dynamic_ncols=True):
@@ -41,13 +44,11 @@ def main():
                 ),
             ) > 2:
                 continue
-            image_file = study_dir / f'{series_num}.nii'
-            if not is_axial(image_file):
-                continue
             meta = json.loads(meta_file.read_bytes())
             assert series_num == meta['SeriesNumber']
-            if not check_modality(meta):
+            if not check_meta(meta):
                 continue
+            image_file = study_dir / f'{series_num}.nii'
             dst_study_dir = dst_dir / study_dir.name
             dst_study_dir.mkdir(exist_ok=True)
             (dst_study_dir / image_file.name).hardlink_to(image_file)
