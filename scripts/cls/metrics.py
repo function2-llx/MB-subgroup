fix radiomics; add heat map; fix metrics calculation

scripts/cls/metrics.py
@@ -4,6 +4,8 @@ from functools import partial
 from pathlib import Path
 from typing import TypeAlias
 
+import itertools as itt
+
 from datasets import ClassLabel
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
@@ -37,13 +39,6 @@ def compute(df: pd.DataFrame, subgroups: ClassLabel, seed: int | None = None):
             for _, group in df.groupby('true')
         ]
         df = pd.concat(groups)
-        # for subgroup in subgroups.names:
-        #     class_data = df.groupby('true').
-        # while True:
-        #     sampled_df = df.sample(frac=1, random_state=rng, replace=True)
-        #     if len(sampled_df['true'].unique()) == subgroups.num_classes:
-        #         df = sampled_df
-        #         break
     label = torch.tensor(df['true'].map(subgroups.str2int).to_numpy())
     pred = torch.tensor(df['pred'].map(subgroups.str2int).to_numpy())
     prob = torch.tensor(np.stack([df[subgroup].to_numpy() for subgroup in subgroups.names], axis=-1))
@@ -71,16 +66,18 @@ def compute(df: pd.DataFrame, subgroups: ClassLabel, seed: int | None = None):
             report.at['micro', name] = metric.compute().item()
     return report
 
-def run(args: Namespace, df: pd.DataFrame, group: MBGroup | None = None):
-    df_index = df['split'] == 'test'
+def run(config_name: str, output_root: Path, split: str, group: MBGroup | None = None):
+    output_dir = output_root / split / config_name
+    df = pd.read_excel(
+        f'MB-data/outputs/{split}/{config_name}.xlsx',
+        dtype={'case': pd.StringDtype()},
+    ).set_index('case')
     if group is not None:
-        df_index &= test_plan[MBDataKey.GROUP] == group
-    df = df[df_index]
+        df = df[df['group'] == group]
+        output_dir = output_dir / group
     all_subgroups = df['true']
     subgroups = ClassLabel(names=sorted(np.unique(all_subgroups).tolist(), key=lambda x: SUBGROUPS.index(x)))
 
-    group_name = 'all' if group is None else group
-    output_dir: Path = args.output_dir / group_name
     output_dir.mkdir(parents=True, exist_ok=True)
 
     report = compute(df, subgroups)
@@ -89,6 +86,8 @@ def run(args: Namespace, df: pd.DataFrame, group: MBGroup | None = None):
         partial(compute, df, subgroups),
         np.random.SeedSequence(42).generate_state(n_bootstraps).tolist(),
         max_workers=8,
+        chunksize=50,
+        dynamic_ncols=True,
     )
     bs_metrics = np.stack([bs_report.to_numpy() for bs_report in bs_reports])
     np.save(output_dir / 'bs_metrics.npy', bs_metrics)
@@ -113,12 +112,6 @@ def run(args: Namespace, df: pd.DataFrame, group: MBGroup | None = None):
     plt.rcParams["font.family"] = "Arial"
     plt.rcParams["font.size"] = 12
     fig, ax = plt.subplots(1, 1, figsize=(12, 12))
-    # if subgroups.num_classes == 4:
-    #     fig, axes = plt.subplots(2, 2, figsize=(12.2, 12))
-    # else:
-    #     fig, axes = plt.subplots(1, 3, figsize=(18.3, 6))
-    #     subgroup_ids = [SUBGROUPS.index(subgroup) for subgroup in subgroups.names]
-    # fig.suptitle(group_name, size=32)
     prob = torch.tensor(np.stack([df[subgroup].to_numpy() for subgroup in subgroups.names], axis=-1))
     label = torch.tensor(df['true'].map(subgroups.str2int).to_numpy())
     fig.set_facecolor('lightgray')
@@ -146,15 +139,16 @@ def run(args: Namespace, df: pd.DataFrame, group: MBGroup | None = None):
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('file', type=Path)
-    parser.add_argument('--output_dir', type=Path, default='plot')
-    parser.add_argument('--all', action='store_true')
+    parser.add_argument('--output_root', type=Path, default='MB-data/metrics')
     args = parser.parse_args()
-    case_outputs = pd.read_excel(args.file, sheet_name='4way', dtype={'case': 'string'}).set_index('case')
-    run(args, case_outputs)
-    if not args.all:
-        run(args, case_outputs, MBGroup.CHILD)
-        run(args, case_outputs, MBGroup.ADULT)
+    for config_name, split in itt.product(
+        ['mbmf', 'scratch', 'radiomics'],
+        ['test', 'val'],
+    ):
+        run(config_name, args.output_root, split)
+        if config_name == 'mbmf' and split == 'test':
+            run(config_name, args.output_root, split, MBGroup.CHILD)
+            run(config_name, args.output_root, split, MBGroup.ADULT)
 
 if __name__ == '__main__':
     main()

scripts/cls/stat-cmp.py
@@ -1,4 +1,5 @@
 from enum import StrEnum
+from luolib.types import PathLike
 
 import numpy as np
 import pandas as pd
@@ -7,14 +8,25 @@ from rpy2.robjects.packages import importr
 from statsmodels.stats.contingency_tables import mcnemar
 
 from mbs.utils.enums import SUBGROUPS
-from _misc import read_case_outputs
 
 pROC = importr('pROC')
+
+def read_case_outputs(filepath: PathLike):
+    df = pd.read_excel(filepath, dtype={'case': 'string'}).set_index('case')
+    return df
 
 class Metric(StrEnum):
     SEN = 'sensitivity'
     SPE = 'specificity'
     ACC = 'accuracy'
+
+def report_metric_map(metric: str):
+    return {
+        'sensitivity': 'sen',
+        'specificity': 'spe',
+        'accuracy': 'acc',
+        'auc': 'auroc',
+    }[metric]
 
 def mcnemar_table(p1: np.ndarray, p2: np.ndarray):
     table = np.array([
@@ -24,19 +36,41 @@ def mcnemar_table(p1: np.ndarray, p2: np.ndarray):
 
     return table, table[0, 1] + table[1, 0] < 25
 
-def main():
-    rng = np.random.default_rng(42)
-    o1 = read_case_outputs('case-output.xlsx')
-    # o2 = read_case_outputs('case-output-scratch.xlsx')
-    o2 = read_case_outputs('radiomics/case-output/MLP.xlsx')
+metrics_list = [
+    'f1', 'sensitivity', 'specificity', 'accuracy', 'auc'
+]
+
+def get_cls_index(cls: str):
+    if cls == 'macro':
+        return len(SUBGROUPS)
+    else:
+        return SUBGROUPS.index(cls)
+
+def compare_predictions(split: str, m1: str, m2: str) -> pd.DataFrame:
+    o1 = read_case_outputs(f'MB-data/outputs/{split}/{m1}.xlsx')
+    o2 = read_case_outputs(f'MB-data/outputs/{split}/{m2}.xlsx')
     assert ((true := o1['true'].to_numpy()) == o2['true'].to_numpy()).all()
     p1 = o1['pred'].to_numpy()
     p2 = o2['pred'].to_numpy()
+    diff_bs = np.load(f'MB-data/metrics/{split}/{m1}/bs_metrics.npy') - np.load(f'MB-data/metrics/{split}/{m2}/bs_metrics.npy')
+    ci = np.stack(
+        [
+            np.percentile(diff_bs, q=2.5, axis=0),
+            np.percentile(diff_bs, q=97.5, axis=0),
+        ],
+        axis=-1,
+    )
+    report1 = pd.read_excel(f'MB-data/metrics/{split}/{m1}/report.xlsx', index_col=0)
+    report2 = pd.read_excel(f'MB-data/metrics/{split}/{m2}/report.xlsx', index_col=0)
+
     results = []
+    
     for cls in SUBGROUPS:
         cls_mask = true == cls
         p1b = (p1 == cls) == cls_mask
         p2b = (p2 == cls) == cls_mask
+        
+        # McNemar's test for different metrics
         for metric in Metric:
             match metric:
                 case Metric.SEN:
@@ -45,30 +79,55 @@ def main():
                     sub_idx = ~cls_mask
                 case Metric.ACC:
                     sub_idx = slice(None)
-            # sub_true = true[sub_idx]
+                    
             table, exact = mcnemar_table(p1b[sub_idx], p2b[sub_idx])
             result = mcnemar(table, exact=False, correction=table[1, 0] == table[0, 1])
             p = result.pvalue
-            # print(table)
-            print(cls, metric, result.statistic, p)
             results.append({
                 'cls': cls,
                 'metric': metric,
+                'value': report2.loc[cls, report_metric_map(metric)],
+                'diff': report1.loc[cls, report_metric_map(metric)] - report2.loc[cls, report_metric_map(metric)],
                 'p': p,
             })
+        
+        # AUC comparison using DeLong's test
         true_r = robjects.FloatVector(cls_mask)
         prob1_r = robjects.FloatVector(o1[cls].to_numpy())
         prob2_r = robjects.FloatVector(o2[cls].to_numpy())
         roc_1 = pROC.roc(true_r, prob1_r)
         roc_2 = pROC.roc(true_r, prob2_r)
+        print(cls, roc_1.rx2('auc')[0], roc_2.rx2('auc')[0])
         test_result = pROC.roc_test(roc_1, roc_2, method='delong')
         print('auc', p := test_result.rx2('p.value')[0])
         results.append({
             'cls': cls,
             'metric': 'auc',
+            'value': report2.loc[cls, 'auroc'],
+            'diff': report1.loc[cls, 'auroc'] - report2.loc[cls, 'auroc'],
             'p': p,
         })
-    pd.DataFrame.from_records(results).to_excel('stat-results.xlsx')
+
+    for metric in metrics_list[1:]:
+        results.append({
+            'cls': 'macro',
+            'metric': metric,
+            'value': report2.loc['macro', report_metric_map(metric)],
+            # no p value for macro-avg metrics
+        })
+
+    df = pd.DataFrame.from_records(results)
+    df['ci-0'] = df.apply(lambda r: ci[get_cls_index(r['cls']), metrics_list.index(r['metric']), 0], axis=1)
+    df['ci-1'] = df.apply(lambda r: ci[get_cls_index(r['cls']), metrics_list.index(r['metric']), 1], axis=1)
+    df['diff'] = df.apply(lambda r: report1.loc[r['cls'], report_metric_map(r['metric'])] - r['value'], axis=1)
+    return df
+
+def main():
+    for split in ['test', 'val']:
+        for i, target in enumerate(['radiomics', 'scratch']):
+            results_df = compare_predictions(split, 'mbmf', target)
+            with pd.ExcelWriter(f'MB-data/cmp/{split}.xlsx', mode='a' if i else 'w', engine='openpyxl') as writer:
+                results_df.to_excel(writer, sheet_name=target, index=False)
 
 if __name__ == '__main__':
     main()